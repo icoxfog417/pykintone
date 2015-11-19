57b947c04f827ee2cc622f4779077ca34ddaf8ac
icoxfog417 <icoxfog417@yahoo.co.jp>
2015-11-19 18:12:26 +0900

    create base structure class

--- pykintone/model.py
+++ pykintone/model.py
@@ -12,45 +12,19 @@ class kintoneModel(ps.kintoneStructure):
 
     @classmethod
     def record_to_model(cls, record_json):
-        instance = cls()
-        is_set = False
-        properties = cls._get_property_names(instance)
-
-        def get_property_detail(name):
-            p = [p for p in instance._property_details if p.name == name or p.field_name == name]
-            return None if len(p) == 0 else p[0]
-
-        for k in record_json:
-            field = record_json[k]
-            pd = get_property_detail(k)
-            pn = k if not pd else pd.to_property_name(k)
-            if pn in properties:
-                value = instance.field_to_property(field, pd)
-                setattr(instance, pn, value)
-                is_set = True
-
-        return instance if is_set else None
+        def get_value_and_type(f): return f["value"], f["type"]
+        return cls._deserialize(record_json, get_value_and_type)
 
     def to_record(self):
-        record = {}
-        properties = self._get_property_names(self)
+        def convert_to_key_and_value(field_name, value, property_detail=None):
+            key = field_name
+            formatted = {
+                "value": value
+            }
+            if field_name in ["$id", "$revision"]:
+                if value > -1:
+                    key = field_name[1:] # escape $
 
-        def get_property_detail(name):
-            p = [p for p in self._property_details if p.name == name]
-            return None if len(p) == 0 else p[0]
+            return key, formatted
 
-        for p in properties:
-            pd = get_property_detail(p)
-            value = self.property_to_field(p, pd)
-            fn = p if not pd else pd.to_field_name()
-            if value is not None and not (pd and pd.unsent):
-                formatted = {
-                    "value": value
-                }
-                if fn in ["$id", "$revision"]:
-                    if value > -1:
-                        record[fn[1:]] = formatted  # escape $
-                else:
-                    record[fn] = formatted
-
-        return record
+        return self._serialize(convert_to_key_and_value)
--- pykintone/structure.py
+++ pykintone/structure.py
@@ -1,4 +1,5 @@
 import inspect
+import re
 from enum import Enum
 from collections import namedtuple
 from datetime import datetime
@@ -6,6 +7,7 @@ from pykintone.account import kintoneService as ks
 
 
 class kintoneStructure(object):
+
     def __init__(self):
         self._property_details = []
 
@@ -20,35 +22,66 @@ class kintoneStructure(object):
         return names
 
     @classmethod
-    def field_to_property(cls, field, property_detail=None):
-        value = field["value"]
+    def _deserialize(cls, json_body, get_value_and_type):
+        """
+        deserialize json to model
+        :param json_body: json data
+        :param get_value_and_type: function(f: json_field) -> value, field_type_string(see FieldType)
+        :return:
+        """
+
+        instance = cls()
+        is_set = False
+        properties = cls._get_property_names(instance)
+
+        def get_property_detail(name):
+            p = [p for p in instance._property_details if p.name == name or p.field_name == name]
+            return None if len(p) == 0 else p[0]
+
+        for k in json_body:
+            field = json_body[k]
+            pd = get_property_detail(k)
+            pn = k if not pd else pd.to_property_name(k)
+            if pn in properties:
+                v, t = get_value_and_type(field)
+                value = instance._field_to_property(v, t, pd)
+                setattr(instance, pn, value)
+                is_set = True
+
+        return instance if is_set else None
+
+    @classmethod
+    def _field_to_property(cls, field_value, field_type=None, property_detail=None):
+        value = field_value
 
         # configure property's field type
         # from user definition
-        field_type = None if not property_detail else property_detail.field_type
+        _field_type = None if not property_detail else property_detail.field_type
         # from type value in field
-        if not field_type:
-            f = [e for e in list(FieldType) if e.value == field["type"]]
+        if not _field_type and field_type:
+            f = [e for e in list(FieldType) if e.value == field_type]
             if len(f) > 0:
-                field_type = f[0]
+                _field_type = f[0]
 
-        if not field_type:
+        if not _field_type:
             pass
-        elif field_type in (FieldType.ID, FieldType.REVISION, FieldType.RECORD_NUMBER):
+        elif _field_type in (FieldType.ID, FieldType.REVISION, FieldType.RECORD_NUMBER):
             value = int(value)
-        elif field_type == FieldType.NUMBER:
+        elif _field_type == FieldType.NUMBER:
             value = float(value)
-        elif field_type == FieldType.DATE:
+        elif _field_type == FieldType.DATE:
             value = ks.value_to_date(value)
-        elif field_type == FieldType.TIME:
+        elif _field_type == FieldType.TIME:
             value = ks.value_to_time(value)
-        elif field_type in [FieldType.DATETIME, FieldType.CREATED_TIME, FieldType.UPDATED_TIME]:
+        elif _field_type in [FieldType.DATETIME, FieldType.CREATED_TIME, FieldType.UPDATED_TIME]:
             value = ks.value_to_datetime(value)
-        elif field_type == FieldType.USER_SELECT:
+        elif _field_type == FieldType.TIME_STAMP:
+            value = ks.value_to_timestamp(value)
+        elif _field_type == FieldType.USER_SELECT:
             value = [UserSelect(v["code"], v["name"]) for v in value]
-        elif field_type in [FieldType.CREATOR, FieldType.MODIFIER]:
+        elif _field_type in [FieldType.CREATOR, FieldType.MODIFIER]:
             value = UserSelect(value["code"], value["name"])
-        elif field_type == FieldType.SUBTABLE:
+        elif _field_type == FieldType.SUBTABLE:
             if property_detail and property_detail.sub_type:
                 table = []
                 for r in value:
@@ -56,12 +89,36 @@ class kintoneStructure(object):
                     row.record_id = int(r["id"])
                     table.append(row)
                 value = table
-        elif field_type == FieldType.FILE:
+        elif _field_type == FieldType.FILE:
             pass  # todo: conversion for file
 
         return value
 
-    def property_to_field(self, name, property_detail=None):
+    def _serialize(self, convert_to_key_and_value):
+        """
+        serialize model object to dictionary
+        :param convert_to_key_and_value: function(field_name, value, property_detail) -> key, value
+        :return:
+        """
+
+        serialized = {}
+        properties = self._get_property_names(self)
+
+        def get_property_detail(name):
+            p = [p for p in self._property_details if p.name == name]
+            return None if len(p) == 0 else p[0]
+
+        for p in properties:
+            pd = get_property_detail(p)
+            value = self._property_to_field(p, pd)
+            field_name = p if not pd else pd.to_field_name()
+            if value is not None and not (pd and pd.unsent):
+                key, value = convert_to_key_and_value(field_name, value, pd)
+                serialized[key] = value
+
+        return serialized
+
+    def _property_to_field(self, name, property_detail=None):
         value = getattr(self, name)
         if value is None:
             return None
@@ -80,7 +137,8 @@ class kintoneStructure(object):
             value = ks.date_to_value(value)
         elif field_type == FieldType.TIME:
             value = ks.time_to_value(value)
-        elif field_type in [FieldType.DATETIME, FieldType.CREATED_TIME, FieldType.UPDATED_TIME]:
+        elif field_type in [FieldType.DATETIME, FieldType.CREATED_TIME, FieldType.UPDATED_TIME, FieldType.TIME_STAMP]:
+            # time stamp is same as datetime format (there is no field for timestamp in kintone)
             value = ks.datetime_to_value(value)
         elif field_type == FieldType.USER_SELECT:
             value = [{"code": u.code} for u in value]
@@ -132,10 +190,32 @@ class FieldType(Enum):
     MODIFIER = "MODIFIER"
     FILE = "FILE"
     RECORD_NUMBER = "RECORD_NUMBER"
-    ID = "__ID__"
-    REVISION = "__REVISION__"
     NUMBER = "NUMBER"
     SUBTABLE = "SUBTABLE"
+    CALC = "CALC"
+    CATEGORY = "CATEGORY"
+    CHECK_BOX = "CHECK_BOX"
+    DROP_DOWN = "DROP_DOWN"
+    HR = "HR"
+    LABEL = "LABEL"
+    LINK = "LINK"
+    MULTI_LINE_TEXT = "MULTI_LINE_TEXT"
+    MULTI_SELECT = "MULTI_SELECT"
+    RADIO_BUTTON = "RADIO_BUTTON"
+    RICH_TEXT = "RICH_TEXT"
+    SINGLE_LINE_TEXT = "SINGLE_LINE_TEXT"
+    SPACER = "SPACER"
+    STATUS = "STATUS"
+    STATUS_ASSIGNEE = "STATUS_ASSIGNEE"
+    ID = "__ID__"
+    REVISION = "__REVISION__"
+    TIME_STAMP = "TIME_STAMP"
 
 
 UserSelect = namedtuple("UserSelect", ["code", "name"])
+
+
+class LayoutType(Enum):
+    ROW = "ROW"
+    SUBTABLE = "SUBTABLE"
+    GROUP = "GROUP"
