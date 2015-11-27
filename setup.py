13d5475c6852f3b159ef8f940c11ad350d47a0b9
icoxfog417 <icoxfog417@yahoo.co.jp>
2015-11-27 15:21:35 +0900

    update version

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ setup(
         'PyYAML',
         'requests'
     ],
-    version='0.2.0',
+    version='0.2.0.1',
     description='Python library to access kintone',
     author='icoxfog417',
     author_email='icoxfog417@yahoo.co.jp',
     url='https://github.com/icoxfog417/pykintone',
-    download_url='https://github.com/icoxfog417/pykintone/tarball/0.2.0',
+    download_url='https://github.com/icoxfog417/pykintone/tarball/0.2.0.1',
     keywords=['kintone'],
     classifiers=[],
 )
